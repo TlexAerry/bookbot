2a4479e99c9128e9c0975072b4712318f7f36f78
TlexAerry <alexterry987@proton.me>
2025-02-07 19:25:42 +0000

    commented out old word count function, created new letter count function

--- main.py
+++ main.py
@@ -1,11 +1,23 @@
 def main():
+    """
     with open("books/frankenstein.txt") as f:
         file_contents = f.read()
 
     words = file_contents.split()
 
     print(len(words))
+    """
+    
+    with open("books/frankenstein.txt") as f:
+        file_contents = f.read()
 
+    letter_dict ={}
+    file_contents = file_contents.lower()
+    for letter in file_contents:
+        if letter in letter_dict:
+            letter_dict[letter] += 1
+        else:
+            letter_dict[letter] = 1
 
-
+    print(letter_dict)
 main()
